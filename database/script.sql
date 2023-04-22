5d869e82fd0c920ee26adc19971d2e3624956cff
DiogoAlexandreOliveiraDaSilva <afkflex@gmail.com>
2023-04-22 10:27:05 +0100

    changed ticket and hastag sql

--- database/script.sql
+++ database/script.sql
@@ -11,91 +11,65 @@ DROP TABLE IF EXISTS DEPARTMENTS;
 
 CREATE TABLE CLIENTS (
     client_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username VARCHAR
-(255) NOT NULL,
-    password VARCHAR
-(255) NOT NULL,
-    email VARCHAR
-(255) NOT NULL
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE DEPARTMENTS (
     department_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    department_name VARCHAR
-(255) NOT NULL
+    department_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE AGENTS (
     agent_id INTEGER PRIMARY KEY AUTOINCREMENT,
     client_id INTEGER NOT NULL,
-    department_id INTEGER ,
-    FOREIGN KEY
-(client_id) REFERENCES CLIENTS
-(client_id),
-    FOREIGN KEY
-(department_id) REFERENCES DEPARTMENTS
-(department_id)
+    department_id INTEGER,
+    FOREIGN KEY(client_id) REFERENCES CLIENTS(client_id),
+    FOREIGN KEY(department_id) REFERENCES DEPARTMENTS(department_id)
 );
 
 CREATE TABLE ADMINS (
     admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
     client_id INTEGER NOT NULL,
     agent_id INTEGER NOT NULL,
-    FOREIGN KEY
-(agent_id) REFERENCES AGENTS
-(agent_id)
+    FOREIGN KEY(agent_id) REFERENCES AGENTS(agent_id)
 );
 
-CREATE TABLE FAQS(
+CREATE TABLE FAQS (
     faq_id INTEGER PRIMARY KEY AUTOINCREMENT,
     department_id INTEGER NOT NULL,
-    question VARCHAR
-(255) NOT NULL,
-    answer VARCHAR
-(255),
-    FOREIGN KEY
-(department_id) REFERENCES DEPARTMENTS
-(department_id)
+    question VARCHAR(255) NOT NULL,
+    answer VARCHAR(255),
+    FOREIGN KEY(department_id) REFERENCES DEPARTMENTS(department_id)
 );
 
-CREATE TABLE TICKETS(
+CREATE TABLE TICKETS (
     ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
     agent_id INTEGER NOT NULL,
     client_id INTEGER NOT NULL,
     department_id INTEGER NOT NULL,
     status BOOLEAN NOT NULL,
-    FOREIGN KEY
-(department_id) REFERENCES DEPARTMENTS
-(department_id),
-    FOREIGN KEY
-(client_id) REFERENCES CLIENTS
-(client_id),
-    FOREIGN KEY
-(agent_id) REFERENCES AGENTS
-(agent_id)
+    hashtag_id INTEGER,
+    FOREIGN KEY(department_id) REFERENCES DEPARTMENTS(department_id),
+    FOREIGN KEY(client_id) REFERENCES CLIENTS(client_id),
+    FOREIGN KEY(agent_id) REFERENCES AGENTS(agent_id),
+    FOREIGN KEY(hashtag_id) REFERENCES HASHTAGS(hashtag_id)
 );
 
-CREATE TABLE MESSAGES(
+CREATE TABLE MESSAGES (
     message_id INTEGER PRIMARY KEY AUTOINCREMENT,
     ticket_id INTEGER NOT NULL,
-    message_content VARCHAR
-(255) NOT NULL,
+    message_content VARCHAR(255) NOT NULL,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     client_id INTEGER NOT NULL,
-    FOREIGN KEY
-(ticket_id) REFERENCES TICKETS
-(ticket_id),
-    FOREIGN KEY (client_id) REFERENCES CLIENTS(client_id)
+    FOREIGN KEY(ticket_id) REFERENCES TICKETS(ticket_id),
+    FOREIGN KEY(client_id) REFERENCES CLIENTS(client_id)
 );
 
 CREATE TABLE HASHTAGS (
     hashtag_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    hashtag_name VARCHAR
-(255) NOT NULL,
-    ticket_id INTEGER NOT NULL,
-    FOREIGN KEY
-(ticket_id) REFERENCES TICKETS
-(ticket_id)
+    hashtag_name VARCHAR(255) NOT NULL
 );
 
 -- Inserting admin account into the database
