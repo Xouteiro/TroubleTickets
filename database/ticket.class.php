a1b1705cf3d4c7d630fad232b9f1bfded849c4f4
DiogoAlexandreOliveiraDaSilva <afkflex@gmail.com>
2023-04-22 15:03:27 +0100

    Fixed

--- database/ticket.class.php
+++ database/ticket.class.php
@@ -9,23 +9,21 @@ class Ticket
     public int $client_id;
     public int $department_id;
     public bool $status;
-    public string $hashtag_id;
 
-    public function __construct(int $id, int $agent_id, int $client_id, int $department_id, bool $status, string $hashtag_id)
+    public function __construct(int $id, int $agent_id, int $client_id, int $department_id, bool $status)
     {
         $this->id = $id;
         $this->agent_id = $agent_id;
         $this->client_id = $client_id;
         $this->department_id = $department_id;
         $this->status = $status;
-        $this->hashtag_id = $hashtag_id;
     }
 
-    static function updateTicket(PDO $db, int $id, int $agent_id, int $client_id, int $department_id, bool $status, string $hashtag_id): bool
+    static function updateTicket(PDO $db, int $id, int $agent_id, int $client_id, int $department_id, bool $status): bool
     {
-        $stmt = $db->prepare('UPDATE TICKETS SET agent_id = ?, client_id = ?, department_id = ?, status = ?, hashtag_id = ? WHERE ticket_id = ?');
+        $stmt = $db->prepare('UPDATE TICKETS SET agent_id = ?, client_id = ?, department_id = ?, status = ? WHERE ticket_id = ?');
         try {
-            $stmt->execute(array($agent_id, $client_id, $department_id, $status, $hashtag_id, $id));
+            $stmt->execute(array($agent_id, $client_id, $department_id, $status, $id));
             return true;
         } catch (PDOException $e) {
             return false;
@@ -34,7 +32,7 @@ class Ticket
 
     static function getTickets(PDO $db, int $count): array
     {
-        $stmt = $db->prepare('SELECT ticket_id, agent_id, client_id, department_id, status, hashtag_id FROM TICKETS LIMIT ?');
+        $stmt = $db->prepare('SELECT ticket_id, agent_id, client_id, department_id, status FROM TICKETS LIMIT ?');
         $stmt->execute(array($count));
 
         $tickets = array();
@@ -44,8 +42,7 @@ class Ticket
                 $ticket['agent_id'],
                 $ticket['client_id'],
                 $ticket['department_id'],
-                $ticket['status'],
-                $ticket['hashtag_id']
+                $ticket['status']
             );
         }
         return $tickets;
@@ -53,7 +50,7 @@ class Ticket
 
     static function getTicketById(PDO $db, int $id): ?Ticket
     {
-        $stmt = $db->prepare('SELECT ticket_id, agent_id, client_id, department_id, status, hashtag_id FROM TICKETS WHERE ticket_id = ?');
+        $stmt = $db->prepare('SELECT ticket_id, agent_id, client_id, department_id, status FROM TICKETS WHERE ticket_id = ?');
         $stmt->execute(array($id));
 
         $ticket = $stmt->fetch();
@@ -63,19 +60,18 @@ class Ticket
                 $ticket['agent_id'],
                 $ticket['client_id'],
                 $ticket['department_id'],
-                $ticket['status'],
-                $ticket['hashtag_id']
+                $ticket['status']
             );
         } else {
             return null;
         }
     }
 
-    static function newTicket(PDO $db, int $agent_id, int $client_id, int $department_id, bool $status, string $hashtag_id): bool
+    static function newTicket(PDO $db, int $agent_id, int $client_id, int $department_id, bool $status): bool
     {
-        $stmt = $db->prepare('INSERT INTO TICKETS (agent_id, client_id, department_id, status, hashtag_id) VALUES (?, ?, ?, ?, ?)');
+        $stmt = $db->prepare('INSERT INTO TICKETS (agent_id, client_id, department_id, status) VALUES (?, ?, ?, ?)');
         try {
-            $stmt->execute(array($agent_id, $client_id, $department_id, $status, $hashtag_id));
+            $stmt->execute(array($agent_id, $client_id, $department_id, $status));
             return true;
         } catch (PDOException $e) {
             return false;
