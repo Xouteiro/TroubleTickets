c332eca1d1b348711f52db3288daae485d1d90e2
couto202006526 <93646403+couto202006526@users.noreply.github.com>
2023-05-15 19:12:59 +0100

    Update new_ticket_template.php

--- templates/new_ticket_template.php
+++ templates/new_ticket_template.php
@@ -12,23 +12,22 @@ function output_new_ticket_page(Session $session)
 
 ?>
     <section id='new-ticket-form' class='new-ticket-form'>
-    <h2>New Ticket</h2>
+    <h2 style="color: white;">Create your ticket</h2>
     <form id="new-ticket" action="../actions/action_create_ticket.php" method="post">
-    <label for="departments">Department:</label>
-    <select id="departments" name="departments">
+    <label for="departments" style="color: white;">Department:</label>
+    <select id="departments" name="departments" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width:657px; height: 30px;">
         <?php foreach($departments as $department){?>
             <option value="<?php echo $department->name?>"><?php echo $department->name?></option>
        <?php } 
         ?>
     </select>
-    <label for="ticket-title">Title:</label>
-    <input type="text" id="ticket-title" name="ticket-title" placeholder="Small description of the problem (Max: 30 characters)">
-    <label for="ticket-message">Message:</label>
-    <textarea id="ticket-message" name="ticket-message" placeholder="Detailed description of the problem." rows="10" cols="30"></textarea>
-    <button type="submit" name="submit" class="ticket">Create Ticket</button>
+    <label for="ticket-title" style="color: white;">Title:</label>
+    <input type="text" id="ticket-title" name="ticket-title" placeholder="Small description of the problem (Max: 30 characters)" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width: 650px; height: 30px;">
+    <label for="ticket-message" style="color: white;">Message:</label>
+    <textarea id="ticket-message" name="ticket-message" placeholder="Detailed description of the problem." rows="10" cols="30" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width: 650px; height: 150px;"></textarea>
+    <button type="submit" name="submit" class="ticket" style="color: white; background-color: #F47458; border-radius: 5px; border-color: #F47458; width: 657px; height: 30px; margin-top: 10px;">Create Ticket</button>
     </form>
     </section>
-    
 
 <?php
 }
