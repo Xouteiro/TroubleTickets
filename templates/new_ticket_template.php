80fced1cc387878ada98fd43bee099da509a0f1e
couto202006526 <93646403+couto202006526@users.noreply.github.com>
2023-05-15 21:04:00 +0100

    Update new_ticket_template.php

--- templates/new_ticket_template.php
+++ templates/new_ticket_template.php
@@ -15,19 +15,20 @@ function output_new_ticket_page(Session $session)
     <h2 style="color: white;">Create your ticket</h2>
     <form id="new-ticket" action="../actions/action_create_ticket.php" method="post">
     <label for="departments" style="color: white;">Department:</label>
-    <select id="departments" name="departments" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width:657px; height: 30px;">
+    <select id="departments" name="departments" class="selectdepartment">
         <?php foreach($departments as $department){?>
             <option value="<?php echo $department->name?>"><?php echo $department->name?></option>
        <?php } 
         ?>
     </select>
     <label for="ticket-title" style="color: white;">Title:</label>
-    <input type="text" id="ticket-title" name="ticket-title" placeholder="Small description of the problem (Max: 30 characters)" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width: 650px; height: 30px;">
+    <input type="text" id="ticket-title" name="ticket-title" placeholder="Small description of the problem (Max: 30 characters)" class="input-ticket-title">
     <label for="ticket-message" style="color: white;">Message:</label>
-    <textarea id="ticket-message" name="ticket-message" placeholder="Detailed description of the problem." rows="10" cols="30" style="color: white; background-color: #CFCFCF; border-radius: 5px; border-color: #CFCFCF; width: 650px; height: 150px;"></textarea>
-    <button type="submit" name="submit" class="ticket" style="color: white; background-color: #F47458; border-radius: 5px; border-color: #F47458; width: 657px; height: 30px; margin-top: 10px;">Create Ticket</button>
+    <textarea id="ticket-message" name="ticket-message" placeholder="Detailed description of the problem." rows="10" cols="30" class="textarea-ticket-message"></textarea>
+    <button type="submit" name="submit" class="button-create-ticket">Create Ticket</button>
     </form>
     </section>
+    
 
 <?php
 }
