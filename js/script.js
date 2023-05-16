20d76f915d221dbd1fd20efaab0f24ec553b2d62
DiogoAlexandreOliveiraDaSilva <afkflex@gmail.com>
2023-05-16 15:14:43 +0100

    fixed stuff

--- js/script.js
+++ js/script.js
@@ -6,59 +6,59 @@ p.textContent = 'No tickets here yet!';
 
 
 if (checkbox && tickets && p) {
-    checkbox.addEventListener('change', function () {
-        let flag = 0;
-        p.remove();
+  checkbox.addEventListener('change', function () {
+    let flag = 0;
+    p.remove();
 
-        if (this.checked) {
-            const h3 = document.querySelector('h3[data-agent-dep]');
-            const agentDep = h3.getAttribute('data-agent-dep');
-            for (let i = 0; i < tickets.length; i++) {
-                const h5 = tickets[i].querySelector('h5[data-ticket-dep]');
-                const ticketDep = h5.getAttribute('data-ticket-dep');
-                console.log(ticketDep);
-                if (ticketDep === agentDep) {
-                    tickets[i].style.display = 'flex';
-                    flag = 1;
-                } else {
-                    tickets[i].style.display = 'none';
-                }
-            }
+    if (this.checked) {
+      const h3 = document.querySelector('h3[data-agent-dep]');
+      const agentDep = h3.getAttribute('data-agent-dep');
+      for (let i = 0; i < tickets.length; i++) {
+        const h5 = tickets[i].querySelector('h5[data-ticket-dep]');
+        const ticketDep = h5.getAttribute('data-ticket-dep');
+        console.log(ticketDep);
+        if (ticketDep === agentDep) {
+          tickets[i].style.display = 'flex';
+          flag = 1;
         } else {
-            for (let i = 0; i < tickets.length; i++) {
-                tickets[i].style.display = 'flex';
-                flag = 1;
-            }
+          tickets[i].style.display = 'none';
         }
+      }
+    } else {
+      for (let i = 0; i < tickets.length; i++) {
+        tickets[i].style.display = 'flex';
+        flag = 1;
+      }
+    }
 
-        if (flag === 0) {
-            document.getElementById('unassigned-tickets').appendChild(p);
-        }
-    })
+    if (flag === 0) {
+      document.getElementById('unassigned-tickets').appendChild(p);
+    }
+  })
 }
 
 
 const removeButtons = document.querySelectorAll('.remove-hashtag');
 
 if (removeButtons) {
-    removeButtons.forEach(button => {
-        button.addEventListener('click', function () {
+  removeButtons.forEach(button => {
+    button.addEventListener('click', function () {
 
-            var hashtagId = button.getAttribute("data-name");
-            var ticketId = document.querySelector("h3[data-ticketid]").getAttribute("data-ticketid");
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "../actions/action_remove_hashtag.php", true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.send("ticket_id=" + encodeURIComponent(ticketId) + "&hashtag_id=" + encodeURIComponent(hashtagId));
-            button.remove();
-        })
+      var hashtagId = button.getAttribute("data-name");
+      var ticketId = document.querySelector("h3[data-ticketid]").getAttribute("data-ticketid");
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "../actions/action_remove_hashtag.php", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.send("ticket_id=" + encodeURIComponent(ticketId) + "&hashtag_id=" + encodeURIComponent(hashtagId));
+      button.remove();
     })
+  })
 }
 
 const newHashtag = document.getElementById('new-hashtag');
 
 if (newHashtag) {
-  newHashtag.addEventListener('keyup', function(event) {
+  newHashtag.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
       let hashtagName = newHashtag.value;
       hashtagName = hashtagName.replace(/#/g, ''); // remove any "#" symbols except for the first one
@@ -108,7 +108,7 @@ function resizeInput(input) {
   input.style.width = 7 + input.value.length + "ch";
 }
 
-if(newHashtag){
+if (newHashtag) {
   resizeInput(newHashtag);
   newHashtag.addEventListener('keyup', function () {
     resizeInput(this);
@@ -119,9 +119,8 @@ const departments = document.querySelectorAll('.departments h3[data-dep-id]');
 const faqs = document.querySelectorAll('.question div[data-faq-id]');
 
 
-if (faqs && departments ) {
-  alert('in');
-   departments.forEach(dep => {
+if (faqs && departments) {
+  departments.forEach(dep => {
     dep.addEventListener('click', function () {
       const depId = dep.getAttribute('data-dep-id');
       alert(depId);
@@ -136,6 +135,6 @@ if (faqs && departments ) {
       })
     })
 
-})
+  })
 }
 
